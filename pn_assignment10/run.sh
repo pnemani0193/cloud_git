a99ec7f7d40c1e7846599ff5930a5648f6b4bf94
pnemani0193 <pardhasaradhisastry.nemani@valpo.edu>
2023-03-07 12:52:36 -0500

    fourth commit

--- pn_assignment10/run.sh
+++ pn_assignment10/run.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 echo $2 > token-file
-gcloud compute instances create pn-instance-1 --project=$1 --access-token-file=token-file 
+gcloud compute instances create pn-instance-1 --project=$1 --zone=us-central1-a --network-interface=subnet=pn-subnet1,no-address --tags=web-server,http-server,https-server --access-token-file=token-file
