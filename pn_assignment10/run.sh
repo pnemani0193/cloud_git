bae2631163e4a603efc0ffa63725c6c4c0f3d344
pnemani0193 <pardhasaradhisastry.nemani@valpo.edu>
2023-03-08 00:20:08 -0500

    eighth commit

--- pn_assignment10/run.sh
+++ pn_assignment10/run.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 echo $2 > token-file1
 gcloud config set project pn-project-374416 --access-token-file=token-file1
+gcloud config set account pardhasaradhisastry.nemani@valpo.edu
 gcloud compute instances create pn-instance-1 --access-token-file=token-file1 --project=pn-project-374416 --zone=us-central1-a --network-interface=subnet=pn-subnet1,no-address --tags=web-server,http-server,https-server
